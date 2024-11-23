cbbe7e6fdfaba63b8b70116f71414ea6b8dff9a7
giutala <giuliatala32@gmail.com>
2024-11-23 19:39:30 +0100

    - have a cleaned nasdaq df - added two support datasets - fixed yahoo data retrieval to accomodate 1m fetching limitations - added docstrings to timestamping

--- main.py
+++ main.py
@@ -1,52 +1,31 @@
 from helpermodules.df_dataretrieval import IndexData_Retrieval
-from helpermodules.correlation_analysis import CorrelationAnalysis
+from helpermodules.correlation_study import CorrelationAnalysis
 from models.lstm import prepare_lstm_data, train_lstm, evaluate_model
 import pandas as pd
+import os
 
 df_nasdaq = IndexData_Retrieval(
-    filename='nasdaq_1min',
-    index='NASDAQ-100',
+    filename='cleaned_nasdaq_dataframe',
+    index='NASDAQ 100',
     months=2,
-    frequency='1min',
+    frequency='1m',
     use_yfinance=True
 )
 
 # load data, clean data frame (closing stock prices)
 df_nasdaq.getdata()
+if not os.path.isfile('data/pickle_files/cleaned_nasdaq_dataframe.pkl'):
+    df_nasdaq.clean_df(percentage=20)
 
-# FIXME: if over 10% of data is Nan, drop the ticker; remaining NAN will be replaced with (t-1)
-df_nasdaq.clean_df(5)
+corr_study = CorrelationAnalysis(df_nasdaq.df, df_nasdaq.tickers).winner_rollingcorrelation()
 
-# TODO: readapt correlation analysis to new format df
-corr_study = CorrelationAnalysis(df_nasdaq.df, df_nasdaq.tickers)
-corr_study.get_correlated_stocks(use_pct_change=True)
-corr_study.corr_stocks_pair()
-corr_study.plot_corr_matrix()
-
-# LSTM Integration
-# 3. Caricamento dei dataset 
-df_stock_1 = pd.read_pickle("pickle_files/dataframe_stock_1.pkl")  # Primo titolo 
-df_stock_2 = pd.read_pickle("pickle_files/dataframe_stock_2.pkl")  # Secondo titolo 
-
-
-# 4. Unione dei due DataFrame (basata sull'indice temporale)
-df_combined = pd.concat(
-    [df_stock_1[['GOOGL']], df_stock_2[['GOOG']]], axis=1
-)
-df_combined.fillna(method='ffill', inplace=True)  # Riempie i NaN con il valore precedente
-df_combined.dropna(inplace=True)  # Rimuove righe che contengono NaN
-
-# Rinomina le colonne per maggiore chiarezza
-df_combined.columns = ['Stock_1', 'Stock_2']
-
-# 5. Preparazione dei dati per l'LSTM
 # Divido i dati in training e test set
-train_dataset, test_dataset = prepare_lstm_data(
-    df_combined, 'Stock_1', 'Stock_2', input_sequence_length=24
-)
+# train_dataset, test_dataset = prepare_lstm_data(
+#     df_combined, 'Stock_1', 'Stock_2', input_sequence_length=24
+# )
 
 # 6. Addestramento del modello LSTM
-model = train_lstm(train_dataset, input_sequence_length=24, epochs=10)
+# model = train_lstm(train_dataset, input_sequence_length=24, epochs=10)
 
 # 7. Valutazione del modello
-evaluate_model(model, test_dataset, 'Stock_1')
+# evaluate_model(model, test_dataset, 'Stock_1')
